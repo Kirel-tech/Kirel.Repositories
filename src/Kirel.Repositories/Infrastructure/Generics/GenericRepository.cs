49ed6a79408857587b1248126ea434187e07b5b4
Vlad <vladis.osminin@gmail.com>
2023-01-09 17:45:59 +0300

    created GenericRepository library with EF Core

---
+++ src/Kirel.Repositories/Infrastructure/Generics/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq.Expressions;
+using Kirel.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kirel.Repositories.Infrastructure.Generics;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+{
+    private readonly DbContext _dbContext;
+    
+    public GenericRepository(DbContext context)
+    {
+        _dbContext = context;
+    }
+    
+    public async Task<TEntity> Insert(TEntity entity)
+    {
+         await _dbContext.Set<TEntity>().AddAsync(entity);
+         await _dbContext.SaveChangesAsync();
+         return entity;
+    }
+    public Task<TEntity?> GetById(int id)
+    {
+        var entity = _dbContext.Set<TEntity>().FindAsync(id);
+        return entity.AsTask();
+    }
+    public async Task Delete(int id)
+    {
+        var entity = await GetById(id);
+        if (entity == null) return;
+        _dbContext.Set<TEntity>().Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+    public async Task<TEntity> Update(TEntity entity)
+    {
+        _dbContext.Entry(entity).State = EntityState.Modified;
+        await _dbContext.SaveChangesAsync();
+        return entity;
+    }
+    public async Task<IEnumerable<TEntity>> GetList(Expression<Func<TEntity, bool>> expression)
+    {
+        return await _dbContext.Set<TEntity>().Where(expression).ToListAsync();
+    }
+    public Task<int> Count(Expression<Func<TEntity, bool>> expression)
+    {
+        return _dbContext.Set<TEntity>().CountAsync(expression);
+    }
+    public Task<int> Count()
+    {
+        return _dbContext.Set<TEntity>().CountAsync();
+    }
+}
---
+++ src/Kirel.Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Kirel.Repositories.Interfaces;
+
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    public Task<TEntity> Insert(TEntity entity);
+    public Task<TEntity?> GetById(int id);
+    public Task Delete(int id);
+    public Task<TEntity> Update(TEntity entity);
+    public Task<IEnumerable<TEntity>> GetList(Expression<Func<TEntity, bool>> expression);
+    public Task<int> Count(Expression<Func<TEntity, bool>> expression);
+    public Task<int> Count();
+}
