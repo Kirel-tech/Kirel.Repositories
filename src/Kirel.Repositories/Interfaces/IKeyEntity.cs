3801f7f8dd76cff024836779a92e8ed703bfd2dc
Vlad <vladis.osminin@gmail.com>
2023-01-11 11:27:22 +0300

    modify GenericRepository library with EF Core * added classes for functioning GenericRepository class
    Signed-off-by: Vlad <vladis.osminin@gmail.com>

---
+++ src/Kirel.Repositories/Interfaces/IKeyEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Kirel.Repositories.Interfaces;
+
+/// <summary>
+/// A class containing a field of special entity key
+/// </summary>
+/// <typeparam name="TKey">Type of special entity key</typeparam>
+public interface IKeyEntity<TKey> where TKey :  IComparable, IComparable<TKey>, IEquatable<TKey>
+{
+    /// <summary>
+    /// Id of the entity
+    /// </summary>
+    /// <returns>Key entity</returns>
+    public TKey Id { get; set; }
+}
